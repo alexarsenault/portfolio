88f01d32d4e76c6f5b804904d0508debfc4c04aa
Alexander Arsenault <alexarsenault92@gmail.com>
2022-11-23 08:18:35 -0500

    upgrade next11

--- pages/index.js
+++ pages/index.js
@@ -14,43 +14,43 @@ export default function Home() {
           Alex Arsenault
         </h1>
 
-        <p className={styles.description} style={{ maxWidth: "800px"}}>
+        <div className={styles.description} style={{ maxWidth: "800px"}}>
           <img src="headshot.png" style={{width: "150px", float: "left", borderRadius: "50%", marginRight: "28px"}} /> 
           I'm Alex Arsenault.  I've spent the past eight years building analytics, hardware, and software for space systems. Love everything 
           python, science, engineering, and space! Below is a sampling of some personal projects I've worked on!
-        </p>
+        </div>
 
-        <h2 className={styles.subtitle} style={{ marginTop: "3rem" }}>
+        <div className={styles.subtitle} style={{ marginTop: "3rem" }}>
           Projects
-        </h2>
+        </div>
         <div className={styles.grid}>
-          <a href="/irrigation-system" className={styles.card}>
+          <div href="/irrigation-system" className={styles.card}>
             <h3> Automated Home Irrigation System &rarr;</h3>
             <p>Automated plant watering system using python and raspberry pi.</p>
-          </a>
+          </div>
 
-          <a href="/ml-ds-titanic" className={styles.card}>
+          <div href="/ml-ds-titanic" className={styles.card}>
             <h3>Machine Learning with Titanic Data &rarr;</h3>
             <p>Exploring a Titanic dataset using scikit and seaborn.</p>
-          </a>
+          </div>
 
-          <a
+          <div
             href="/q-learning"
             className={styles.card}
           >
             <h3>Reinforcement Learning and Stock Trading &rarr;</h3>
             <p>Training a Q-learning agent to trade stocks.</p>
-          </a>
+          </div>
 
-          <a
+          <div
             href="/ml-ds-housing"
             className={styles.card}
           >
             <h3>Machine Learning with Housing Data &rarr;</h3>
             <p>Predicting home prices using regression.</p>
-          </a>
+          </div>
 
-          <a
+          <div
             href="/attitude-determination"
             className={styles.card}
           >
@@ -58,13 +58,13 @@ export default function Home() {
             <p>
               Using a hexagonal receiver platform to determine precise GPS positioning.
             </p>
-          </a>
+          </div>
         </div>
 
       </main>
 
       <footer className={styles.footer}>
-        <a
+        <div
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
@@ -78,7 +78,7 @@ export default function Home() {
           <a href="mailto:alex.arsenault@gatech.edu">
             <img src="/mail.svg" alt="" className={styles.logo} />
           </a>
-        </a>
+        </div>
       </footer>
     </div>
   )
