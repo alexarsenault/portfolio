b6d80f68f3f971671da10f8abb77d6acf88c8039
Alexander Arsenault <alexarsenault92@gmail.com>
2022-11-23 08:43:28 -0500

    fixed literally everything

--- pages/attitude-determination.js
+++ pages/attitude-determination.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -22,21 +23,15 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <a href="https://www.linkedin.com/in/alex-arsenault-69941662/">
-              <img src="/linkedin.svg" alt="" className={styles.logo} />
-          </a>
-          <a href="https://github.com/alexarsenault">
-            <img src="/github.svg" alt="" className={styles.logo} />
-          </a>
-          <a href="mailto:alex.arsenault@gatech.edu">
-            <img src="/mail.svg" alt="" className={styles.logo} />
-          </a>
-        </a>
+        <Link href="https://www.linkedin.com/in/alex-arsenault-69941662/">
+            <img src="/linkedin.svg" alt="" className={styles.logo} />
+        </Link>
+        <Link href="https://github.com/alexarsenault">
+          <img src="/github.svg" alt="" className={styles.logo} />
+        </Link>
+        <Link href="mailto:alex.arsenault@gatech.edu">
+          <img src="/mail.svg" alt="" className={styles.logo} />
+        </Link>
       </footer>
     </div>
   )
