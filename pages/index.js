f246a11f4f0879dfaa55e7f2102715f25c7c17b4
Alexander Arsenault <alexarsenault92@gmail.com>
2022-11-23 08:38:37 -0500

    fix all the things

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -24,33 +25,33 @@ export default function Home() {
           Projects
         </div>
         <div className={styles.grid}>
-          <div href="/irrigation-system" className={styles.card}>
+          <Link href="/irrigation-system" className={styles.card}>
             <h3> Automated Home Irrigation System &rarr;</h3>
             <p>Automated plant watering system using python and raspberry pi.</p>
-          </div>
+          </Link>
 
-          <div href="/ml-ds-titanic" className={styles.card}>
+          <Link href="/ml-ds-titanic" className={styles.card}>
             <h3>Machine Learning with Titanic Data &rarr;</h3>
             <p>Exploring a Titanic dataset using scikit and seaborn.</p>
-          </div>
+          </Link>
 
-          <div
+          <Link
             href="/q-learning"
             className={styles.card}
           >
             <h3>Reinforcement Learning and Stock Trading &rarr;</h3>
             <p>Training a Q-learning agent to trade stocks.</p>
-          </div>
+          </Link>
 
-          <div
+          <Link
             href="/ml-ds-housing"
             className={styles.card}
           >
             <h3>Machine Learning with Housing Data &rarr;</h3>
             <p>Predicting home prices using regression.</p>
-          </div>
+          </Link>
 
-          <div
+          <Link
             href="/attitude-determination"
             className={styles.card}
           >
@@ -58,27 +59,21 @@ export default function Home() {
             <p>
               Using a hexagonal receiver platform to determine precise GPS positioning.
             </p>
-          </div>
+          </Link>
         </div>
 
       </main>
 
       <footer className={styles.footer}>
-        <div
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <a href="https://www.linkedin.com/in/alex-arsenault-69941662/">
-              <img src="/linkedin.svg" alt="" className={styles.logo} />
-          </a>
-          <a href="https://github.com/alexarsenault">
-            <img src="/github.svg" alt="" className={styles.logo} />
-          </a>
-          <a href="mailto:alex.arsenault@gatech.edu">
-            <img src="/mail.svg" alt="" className={styles.logo} />
-          </a>
-        </div>
+        <a href="https://www.linkedin.com/in/alex-arsenault-69941662/">
+            <img src="/linkedin.svg" alt="" className={styles.logo} />
+        </a>
+        <a href="https://github.com/alexarsenault">
+          <img src="/github.svg" alt="" className={styles.logo} />
+        </a>
+        <a href="mailto:alex.arsenault@gatech.edu">
+          <img src="/mail.svg" alt="" className={styles.logo} />
+        </a>
       </footer>
     </div>
   )
